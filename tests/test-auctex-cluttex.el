6e0ba6eddb9418c02ed295a5102ff1e326b358a3
Masahiro Nakamura <13937915+tsuu32@users.noreply.github.com>
2020-03-13 11:40:48 +0900

    Add spy test
    And fix TeX-parse-self binding

--- tests/test-auctex-cluttex.el
+++ tests/test-auctex-cluttex.el
@@ -11,58 +11,83 @@
 (describe "Function `auctex-cluttex-mode'"
   :var ((file (expand-file-name "sample/main.tex" auctex-cluttex-test-dir))
         (pdf  (expand-file-name "sample/main.pdf" auctex-cluttex-test-dir))
-        (log  (expand-file-name "sample/main.log" auctex-cluttex-test-dir))
-        (TeX-parse-self t))
-  (before-each
+        (log  (expand-file-name "sample/main.log" auctex-cluttex-test-dir)))
+  (before-all
     (when (file-exists-p pdf)
       (delete-file pdf)))
+  (before-each
+    (spy-on 'auctex-cluttex--TeX-run-ClutTeX :and-call-through))
   (after-each
-   (auctex-cluttex-mode 0))
+    (auctex-cluttex-mode 0))
+
   (it "should enable `auctex-cluttex-mode'"
     (expect auctex-cluttex-mode :to-be nil)
     (auctex-cluttex-mode 1)
     (expect auctex-cluttex-mode :to-be t)
     (expect TeX-command-list :to-contain auctex-cluttex-ClutTeX-command))
+
   (it "should disable `auctex-cluttex-mode'"
     (auctex-cluttex-mode 1)
     (auctex-cluttex-mode 0)
     (expect auctex-cluttex-mode :to-be nil)
     (expect TeX-command-list :not :to-contain auctex-cluttex-ClutTeX-command))
+
   (it "should fail to enable `auctex-cluttex-mode'"
     (let ((exec-path nil))
       (expect (auctex-cluttex-mode 1) :to-throw 'file-error)
       (expect auctex-cluttex-mode :to-be nil)
       (expect TeX-command-list :not :to-contain auctex-cluttex-ClutTeX-command)))
+
   (it "should run cluttex command"
     (auctex-cluttex-mode 1)
-    (let ((buf (find-file-noselect file)))
+    (let* ((TeX-parse-self t)
+           (buf (find-file-noselect file))
+           (TeX-process-asynchronous nil))
       (with-current-buffer buf
-        (setq TeX-process-asynchronous nil)
         (with-simulated-input "ClutTeX RET"
           (call-interactively #'TeX-command-master))
+        (expect 'auctex-cluttex--TeX-run-ClutTeX :to-have-been-called)
         (expect (file-exists-p pdf) :to-be t)
         (expect (file-exists-p log) :to-be nil)
         (kill-buffer buf)))))
 
+
 (describe "Function `auctex-cluttex-set-command-default'"
   :var ((file (expand-file-name "sample/main.tex" auctex-cluttex-test-dir))
         (pdf  (expand-file-name "sample/main.pdf" auctex-cluttex-test-dir))
-        (log  (expand-file-name "sample/main.log" auctex-cluttex-test-dir))
-        (TeX-parse-self t))
+        (log  (expand-file-name "sample/main.log" auctex-cluttex-test-dir)))
   (before-all
     (auctex-cluttex-mode 1))
   (after-all
     (auctex-cluttex-mode 0))
   (before-each
+    (spy-on 'TeX-run-TeX)
+    (spy-on 'auctex-cluttex--TeX-run-ClutTeX :and-call-through)
     (when (file-exists-p pdf)
       (delete-file pdf)))
-  (it "should run cluttex command by default"
-    (let ((buf (find-file-noselect file)))
+
+  (it "should not run cluttex command by default"
+    (let* ((TeX-parse-self t)
+           (buf (find-file-noselect file))
+           (TeX-process-asynchronous nil))
       (with-current-buffer buf
-        (auctex-cluttex-set-command-default)
-        (setq TeX-process-asynchronous nil)
         (with-simulated-input "RET"
           (call-interactively #'TeX-command-master))
+        (expect 'TeX-run-TeX :to-have-been-called)
+        (expect 'auctex-cluttex--TeX-run-ClutTeX :not :to-have-been-called)
+        (kill-buffer buf))))
+
+  (it "should run cluttex command by default"
+    (add-hook 'LaTeX-mode-hook
+              #'auctex-cluttex-set-command-default)
+    (let* ((TeX-parse-self t)
+           (buf (find-file-noselect file))
+           (TeX-process-asynchronous nil))
+      (with-current-buffer buf
+        (with-simulated-input "RET"
+          (call-interactively #'TeX-command-master))
+        (expect 'TeX-run-TeX :not :to-have-been-called)
+        (expect 'auctex-cluttex--TeX-run-ClutTeX :to-have-been-called)
         (expect (file-exists-p pdf) :to-be t)
         (expect (file-exists-p log) :to-be nil)
         (kill-buffer buf)))))
