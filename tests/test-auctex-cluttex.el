d273d7729232a6eaf0fea85524965b33463a720b
Masahiro Nakamura <13937915+tsuu32@users.noreply.github.com>
2020-03-15 11:02:01 +0900

    Update test
    Add SyncTeX support test
    Add one more auctex-cluttex-set-command-default test for
    auctex-cluttex-mode disabled

--- tests/test-auctex-cluttex.el
+++ tests/test-auctex-cluttex.el
@@ -48,25 +48,53 @@
           (call-interactively #'TeX-command-master))
         (expect 'auctex-cluttex--TeX-run-ClutTeX :to-have-been-called)
         (expect (file-exists-p pdf) :to-be t)
-        (expect (file-exists-p log) :to-be nil)
-        (kill-buffer buf)))))
+        (expect (file-exists-p log) :to-be nil))
+      (kill-buffer buf))))
+
+
+(describe "SyncTeX support"
+  :var ((file    (expand-file-name "sample/main.tex" auctex-cluttex-test-dir))
+        (pdf     (expand-file-name "sample/main.pdf" auctex-cluttex-test-dir))
+        (log     (expand-file-name "sample/main.log" auctex-cluttex-test-dir))
+        (synctex (expand-file-name "sample/main.synctex.gz" auctex-cluttex-test-dir)))
+  (before-all
+    (TeX-source-correlate-mode 1)
+    (auctex-cluttex-mode 1)
+    (when (file-exists-p pdf)
+      (delete-file pdf)))
+  (after-all
+    (TeX-source-correlate-mode 0)
+    (auctex-cluttex-mode 0)
+    (when (file-exists-p synctex)
+      (delete-file synctex)))
+
+  (it "should run cluttex command with `--synctex=1' option"
+    (let* ((TeX-parse-self t)
+           (buf (find-file-noselect file))
+           (TeX-process-asynchronous nil))
+      (with-current-buffer buf
+        (with-simulated-input "ClutTeX RET"
+          (call-interactively #'TeX-command-master))
+        (expect (file-exists-p pdf) :to-be t)
+        (expect (file-exists-p synctex) :to-be t)
+        (expect (file-exists-p log) :to-be nil))
+      (kill-buffer buf))))
 
 
 (describe "Function `auctex-cluttex-set-command-default'"
   :var ((file (expand-file-name "sample/main.tex" auctex-cluttex-test-dir))
         (pdf  (expand-file-name "sample/main.pdf" auctex-cluttex-test-dir))
         (log  (expand-file-name "sample/main.log" auctex-cluttex-test-dir)))
-  (before-all
-    (auctex-cluttex-mode 1))
-  (after-all
-    (auctex-cluttex-mode 0))
   (before-each
     (spy-on 'TeX-run-TeX)
     (spy-on 'auctex-cluttex--TeX-run-ClutTeX :and-call-through)
     (when (file-exists-p pdf)
       (delete-file pdf)))
+  (after-each
+    (auctex-cluttex-mode 0))
 
   (it "should not run cluttex command by default"
+    (auctex-cluttex-mode 1)
     (let* ((TeX-parse-self t)
            (buf (find-file-noselect file))
            (TeX-process-asynchronous nil))
@@ -74,10 +102,24 @@
         (with-simulated-input "RET"
           (call-interactively #'TeX-command-master))
         (expect 'TeX-run-TeX :to-have-been-called)
-        (expect 'auctex-cluttex--TeX-run-ClutTeX :not :to-have-been-called)
-        (kill-buffer buf))))
+        (expect 'auctex-cluttex--TeX-run-ClutTeX :not :to-have-been-called))
+      (kill-buffer buf)))
+
+  (it "should not run cluttex command by default if `auctex-cluttex-mode' disabled"
+    (add-hook 'LaTeX-mode-hook
+              #'auctex-cluttex-set-command-default)
+    (let* ((TeX-parse-self t)
+           (buf (find-file-noselect file))
+           (TeX-process-asynchronous nil))
+      (with-current-buffer buf
+        (with-simulated-input "RET"
+          (call-interactively #'TeX-command-master))
+        (expect 'TeX-run-TeX :to-have-been-called)
+        (expect 'auctex-cluttex--TeX-run-ClutTeX :not :to-have-been-called))
+      (kill-buffer buf)))
 
   (it "should run cluttex command by default"
+    (auctex-cluttex-mode 1)
     (add-hook 'LaTeX-mode-hook
               #'auctex-cluttex-set-command-default)
     (let* ((TeX-parse-self t)
@@ -89,5 +131,5 @@
         (expect 'TeX-run-TeX :not :to-have-been-called)
         (expect 'auctex-cluttex--TeX-run-ClutTeX :to-have-been-called)
         (expect (file-exists-p pdf) :to-be t)
-        (expect (file-exists-p log) :to-be nil)
-        (kill-buffer buf)))))
+        (expect (file-exists-p log) :to-be nil))
+      (kill-buffer buf))))
