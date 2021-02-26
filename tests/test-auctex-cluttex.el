9727bca2f52b8a287bb75fffd3278970e5f36979
Masahiro Nakamura <13937915+tsuu32@users.noreply.github.com>
2021-02-26 10:31:56 +0900

    Update tests

--- tests/test-auctex-cluttex.el
+++ tests/test-auctex-cluttex.el
@@ -9,127 +9,81 @@
                                   default-directory))
 
 (describe "Function `auctex-cluttex-mode'"
-  :var ((file (expand-file-name "sample/main.tex" auctex-cluttex-test-dir))
-        (pdf  (expand-file-name "sample/main.pdf" auctex-cluttex-test-dir))
-        (log  (expand-file-name "sample/main.log" auctex-cluttex-test-dir)))
+  :var ((tex (expand-file-name "sample/main.tex" auctex-cluttex-test-dir))
+        (pdf (expand-file-name "sample/main.pdf" auctex-cluttex-test-dir))
+        (log (expand-file-name "sample/main.log" auctex-cluttex-test-dir)))
   (before-all
     (when (file-exists-p pdf)
       (delete-file pdf)))
   (before-each
     (spy-on 'auctex-cluttex--TeX-run-ClutTeX :and-call-through))
-  (after-each
-    (auctex-cluttex-mode 0))
 
   (it "should enable `auctex-cluttex-mode'"
-    (expect auctex-cluttex-mode :to-be nil)
-    (auctex-cluttex-mode 1)
-    (expect auctex-cluttex-mode :to-be t)
-    (expect TeX-command-list :to-contain auctex-cluttex-ClutTeX-command))
+    (with-temp-buffer
+      (expect auctex-cluttex-mode :to-be nil)
+      (auctex-cluttex-mode 1)
+      (expect auctex-cluttex-mode :to-be t)
+      (expect TeX-command-list :to-contain auctex-cluttex-ClutTeX-command)))
 
   (it "should disable `auctex-cluttex-mode'"
-    (auctex-cluttex-mode 1)
-    (auctex-cluttex-mode 0)
-    (expect auctex-cluttex-mode :to-be nil)
-    (expect TeX-command-list :not :to-contain auctex-cluttex-ClutTeX-command))
-
-  (it "should fail to enable `auctex-cluttex-mode'"
-    (let ((exec-path nil))
-      (expect (auctex-cluttex-mode 1) :to-throw 'file-error)
+    (with-temp-buffer
+      (auctex-cluttex-mode 1)
+      (auctex-cluttex-mode 0)
       (expect auctex-cluttex-mode :to-be nil)
       (expect TeX-command-list :not :to-contain auctex-cluttex-ClutTeX-command)))
 
-  (it "should run cluttex command"
-    (auctex-cluttex-mode 1)
+  (it "should run cluttex command synchronously"
     (let* ((TeX-parse-self t)
-           (buf (find-file-noselect file))
+           (buf (find-file-noselect tex))
            (TeX-process-asynchronous nil))
       (with-current-buffer buf
+        (auctex-cluttex-mode 1)
         (with-simulated-input "ClutTeX RET"
           (call-interactively #'TeX-command-master))
         (expect 'auctex-cluttex--TeX-run-ClutTeX :to-have-been-called)
         (expect (file-exists-p pdf) :to-be t)
         (expect (file-exists-p log) :to-be nil))
+      (kill-buffer buf)))
+
+  (it "should not run cluttex command by default"
+    (let* ((buf (find-file-noselect tex)))
+      (with-current-buffer buf
+        (auctex-cluttex-mode 1)
+        (auctex-cluttex-mode 0)
+        (expect TeX-command-default :not :to-equal "ClutTeX"))
+      (kill-buffer buf)))
+
+  (it "should run cluttex command by default"
+    (let* ((buf (find-file-noselect tex)))
+      (with-current-buffer buf
+        (auctex-cluttex-mode 1)
+        (expect TeX-command-default :to-equal "ClutTeX"))
       (kill-buffer buf))))
 
 
 (describe "SyncTeX support"
-  :var ((file    (expand-file-name "sample/main.tex" auctex-cluttex-test-dir))
+  :var ((tex     (expand-file-name "sample/main.tex" auctex-cluttex-test-dir))
         (pdf     (expand-file-name "sample/main.pdf" auctex-cluttex-test-dir))
         (log     (expand-file-name "sample/main.log" auctex-cluttex-test-dir))
         (synctex (expand-file-name "sample/main.synctex.gz" auctex-cluttex-test-dir)))
   (before-all
     (TeX-source-correlate-mode 1)
-    (auctex-cluttex-mode 1)
     (when (file-exists-p pdf)
-      (delete-file pdf)))
-  (after-all
-    (TeX-source-correlate-mode 0)
-    (auctex-cluttex-mode 0)
+      (delete-file pdf))
     (when (file-exists-p synctex)
       (delete-file synctex)))
+  (after-all
+    (TeX-source-correlate-mode 0))
 
   (it "should run cluttex command with `--synctex=1' option"
     (let* ((TeX-parse-self t)
-           (buf (find-file-noselect file))
+           (buf (find-file-noselect tex))
            (TeX-process-asynchronous nil))
       (with-current-buffer buf
+        (auctex-cluttex-mode 1)
         (with-simulated-input "ClutTeX RET"
           (call-interactively #'TeX-command-master))
         (expect (file-exists-p pdf) :to-be t)
         (expect (file-exists-p synctex) :to-be t)
-        (expect (file-exists-p log) :to-be nil))
-      (kill-buffer buf))))
-
-
-(describe "Function `auctex-cluttex-set-command-default'"
-  :var ((file (expand-file-name "sample/main.tex" auctex-cluttex-test-dir))
-        (pdf  (expand-file-name "sample/main.pdf" auctex-cluttex-test-dir))
-        (log  (expand-file-name "sample/main.log" auctex-cluttex-test-dir)))
-  (before-each
-    (spy-on 'TeX-run-TeX)
-    (spy-on 'auctex-cluttex--TeX-run-ClutTeX :and-call-through)
-    (when (file-exists-p pdf)
-      (delete-file pdf)))
-  (after-each
-    (auctex-cluttex-mode 0))
-
-  (it "should not run cluttex command by default"
-    (auctex-cluttex-mode 1)
-    (let* ((TeX-parse-self t)
-           (buf (find-file-noselect file))
-           (TeX-process-asynchronous nil))
-      (with-current-buffer buf
-        (with-simulated-input "RET"
-          (call-interactively #'TeX-command-master))
-        (expect 'TeX-run-TeX :to-have-been-called)
-        (expect 'auctex-cluttex--TeX-run-ClutTeX :not :to-have-been-called))
-      (kill-buffer buf)))
-
-  (it "should not run cluttex command by default if `auctex-cluttex-mode' disabled"
-    (add-hook 'LaTeX-mode-hook
-              #'auctex-cluttex-set-command-default)
-    (let* ((TeX-parse-self t)
-           (buf (find-file-noselect file))
-           (TeX-process-asynchronous nil))
-      (with-current-buffer buf
-        (with-simulated-input "RET"
-          (call-interactively #'TeX-command-master))
-        (expect 'TeX-run-TeX :to-have-been-called)
-        (expect 'auctex-cluttex--TeX-run-ClutTeX :not :to-have-been-called))
-      (kill-buffer buf)))
-
-  (it "should run cluttex command by default"
-    (auctex-cluttex-mode 1)
-    (add-hook 'LaTeX-mode-hook
-              #'auctex-cluttex-set-command-default)
-    (let* ((TeX-parse-self t)
-           (buf (find-file-noselect file))
-           (TeX-process-asynchronous nil))
-      (with-current-buffer buf
-        (with-simulated-input "RET"
-          (call-interactively #'TeX-command-master))
-        (expect 'TeX-run-TeX :not :to-have-been-called)
-        (expect 'auctex-cluttex--TeX-run-ClutTeX :to-have-been-called)
-        (expect (file-exists-p pdf) :to-be t)
         (expect (file-exists-p log) :to-be nil))
       (kill-buffer buf))))
